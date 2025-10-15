b6b62350bbc412157e67902913b7583114e3e219
Shivam Roy <royshivam0123@gmail.com>
2025-10-15 17:03:17 +0530

    Add Frontend

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {data} from '../restApi.json'
+const Menu = () => {
+  return (
+    <>
+      <section className='menu' id='menu'>
+        <div className="container">
+            <div className="heading_section">
+                <h1 className="heading">POPULAR DISHES</h1>
+                <p>At Dineasy, our most-loved dishes are crafted to delight every taste bud. From rich, creamy pastas to perfectly grilled meats, every plate is a favorite. Enjoy bold flavors, fresh ingredients, and recipes that keep our guests coming back for more. Taste the best — only at Dineasy!</p>
+            </div>
+            <div className="dishes_container">
+                {
+                    data[0].dishes.map(element => (
+                        <div className="card" key={element.id}>
+                                <img src={element.image} alt={element.title} />
+                                <h3>{element.title}</h3>
+                                <button>{element.category}</button>
+                        </div>
+                    ))
+                }   
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Menu
